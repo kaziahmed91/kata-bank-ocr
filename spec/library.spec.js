895d262f6ec8e578021e9c8481063470c973556a
“Tinisi” <jgrosz@tinisi.com>
2016-09-17 23:27:17 -0500

    tests of conversions all digits working, fixed test data

--- spec/library.spec.js
+++ spec/library.spec.js
@@ -21,10 +21,92 @@ describe('Recognizer', () => {
     it('sets the stringKeys property with the array of string keys', () => {
       let recognizer = new Recognizer(testData[0].rawRecord);
       recognizer.populateStringKeys();
-      expect(recognizer.stringKeys[0]).toEqual([ ' _  _  _  _  _  _  _  _  _ ', '| || || || || || || || || |', '|_||_||_||_||_||_||_||_||_|' ]);
+      expect(recognizer.stringKeys[0]).toEqual([ ' _ | ||_|', ' _ | ||_|', ' _ | ||_|', ' _ | ||_|', ' _ | ||_|', ' _ | ||_|', ' _ | ||_|', ' _ | ||_|', ' _ | ||_|' ]);
     })
 
   });
+
+  describe('Recognizer#mapStringKeysToIntegers', () => {
+
+    it('returns a translated account number for 000000000', () => {
+      let recognizer = new Recognizer(testData[0].rawRecord);
+      recognizer.populateStringKeys();
+      let result = recognizer.mapStringKeysToIntegers(recognizer.stringKeys[0]);
+      expect(result).toEqual('000000000');
+    });
+
+    it('returns a translated account number for 111111111', () => {
+      let recognizer = new Recognizer(testData[1].rawRecord);
+      recognizer.populateStringKeys();
+      let result = recognizer.mapStringKeysToIntegers(recognizer.stringKeys[0]);
+      expect(result).toEqual('111111111');
+    });
+
+    it('returns a translated account number for 222222222', () => {
+      let recognizer = new Recognizer(testData[2].rawRecord);
+      recognizer.populateStringKeys();
+      let result = recognizer.mapStringKeysToIntegers(recognizer.stringKeys[0]);
+      expect(result).toEqual('222222222');
+    });
+
+   it('returns a translated account number for 333333333', () => {
+      let recognizer = new Recognizer(testData[3].rawRecord);
+      recognizer.populateStringKeys();
+      let result = recognizer.mapStringKeysToIntegers(recognizer.stringKeys[0]);
+      expect(result).toEqual('333333333');
+    });
+
+   it('returns a translated account number for 444444444', () => {
+      let recognizer = new Recognizer(testData[4].rawRecord);
+      recognizer.populateStringKeys();
+      let result = recognizer.mapStringKeysToIntegers(recognizer.stringKeys[0]);
+      expect(result).toEqual('444444444');
+    });
+
+   it('returns a translated account number for 555555555', () => {
+      let recognizer = new Recognizer(testData[5].rawRecord);
+      recognizer.populateStringKeys();
+      let result = recognizer.mapStringKeysToIntegers(recognizer.stringKeys[0]);
+      expect(result).toEqual('555555555');
+    });
+
+   it('returns a translated account number for 666666666', () => {
+      let recognizer = new Recognizer(testData[6].rawRecord);
+      recognizer.populateStringKeys();
+      let result = recognizer.mapStringKeysToIntegers(recognizer.stringKeys[0]);
+      expect(result).toEqual('666666666');
+    });
+
+   it('returns a translated account number for 777777777', () => {
+      let recognizer = new Recognizer(testData[7].rawRecord);
+      recognizer.populateStringKeys();
+      let result = recognizer.mapStringKeysToIntegers(recognizer.stringKeys[0]);
+      expect(result).toEqual('777777777');
+    });
+
+   it('returns a translated account number for 888888888', () => {
+      let recognizer = new Recognizer(testData[8].rawRecord);
+      recognizer.populateStringKeys();
+      let result = recognizer.mapStringKeysToIntegers(recognizer.stringKeys[0]);
+      expect(result).toEqual('888888888');
+    });
+
+   it('returns a translated account number for 999999999', () => {
+      let recognizer = new Recognizer(testData[9].rawRecord);
+      recognizer.populateStringKeys();
+      let result = recognizer.mapStringKeysToIntegers(recognizer.stringKeys[0]);
+      expect(result).toEqual('999999999');
+    });
+
+   it('returns a translated account number for 123456789', () => {
+      let recognizer = new Recognizer(testData[10].rawRecord);
+      recognizer.populateStringKeys();
+      let result = recognizer.mapStringKeysToIntegers(recognizer.stringKeys[0]);
+      expect(result).toEqual('123456789');
+    });
+
+  });
+
 
 
 });
--- src/library.js
+++ src/library.js
@@ -1,14 +1,73 @@
 
+const characterMap = {
+  '\
+   \
+  |\
+  |' : '1',
+  '\
+ _ \
+ _|\
+|_ ' : '2',
+  '\
+ _ \
+ _|\
+ _|' : '3',
+  '\
+   \
+|_|\
+  |' : '4',
+  '\
+ _ \
+|_ \
+ _|' : '5',
+  '\
+ _ \
+|_ \
+|_|' : '6',
+  '\
+ _ \
+  |\
+  |' : '7',
+  '\
+ _ \
+|_|\
+|_|' : '8',
+  '\
+ _ \
+|_|\
+ _|' : '9',
+  '\
+ _ \
+| |\
+|_|' : '0'
+};
+
 class Recognizer {
 
   constructor(rawRecord) {
-    this.rawRecord = rawRecord;
+    this.rawRecord = rawRecord || [];
     this.stringKeys = [];
   } 
 
   populateStringKeys() {
-    let parsedRecord = this.rawRecord;
-    this.stringKeys.push(parsedRecord);
+    let stringKeys = ['','','','','','','','','',];
+    // each field is 3 characters wide
+    let fieldWidth = 3;
+    for ( let row of this.rawRecord) {
+      for ( let index = 0; index < stringKeys.length; index ++) {
+        stringKeys[index] = stringKeys[index] + row.slice(index * fieldWidth, (index * fieldWidth) + fieldWidth);
+      }
+    }
+    this.stringKeys.push(stringKeys);
+  }
+
+  mapStringKeysToIntegers(stringKeys) {
+    let accountNumbers = [];
+    stringKeys.forEach((key, index) => {
+      let translatedNumber = characterMap[key] || '?';
+      accountNumbers[index] = translatedNumber;
+    });
+    return accountNumbers.join('');
   }
 
 }
